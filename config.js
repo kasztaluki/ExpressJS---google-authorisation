e1ba292b9424ecad53c6882ff1c4642f2ca9667a
Łuki Kasztaniewicz <grabowski.lukasz.88@gmail.com>
2018-06-13 00:14:47 +0200

    Add google api

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '842720852755-mi71lbujdr13ag0cmnv31j6vbiipovt0.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'GHr1BBae0Ra1pv2CZJsHgSq0',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
